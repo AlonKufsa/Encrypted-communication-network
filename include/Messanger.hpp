e753511a20c44810086f9c0098d7989a7dc48100
AlonKufsa <alon.gal.kufsa@gmail.com>
2026-07-02 17:50:48 +0300

    Finished messanger and added threading

--- include/Messanger.hpp
+++ include/Messanger.hpp
@@ -2,16 +2,21 @@
 
 #include "Networking.hpp"
 #include <string>
+#include <thread>
 
 class Messanger {
 private:
     const std::string& username;
     Networking& networking;
-public:
-    Messanger(const std::string& username, Networking& networking);
+
+    std::thread inputThread;
 
     void displayText(const std::string& text);
     void displayMessage(const std::string& senderUsername, const std::string& message);
 
     void sendMessage(const std::string& message);
+
+    void listenForInput();
+public:
+    Messanger(const std::string& username, Networking& networking);
 };
--- src/Messanger.cpp
+++ src/Messanger.cpp
@@ -1,13 +1,18 @@
 #include "Networking.hpp"
 #include <Messanger.hpp>
+#include <exception>
 #include <iostream>
+#include <thread>
 
 Messanger::Messanger(const std::string& usernameValue, Networking& networkingValue): username(usernameValue), networking(networkingValue) {
+    networking.connect();
+    networking.listen();
 
+    inputThread = std::thread(&Messanger::listenForInput, this);
 }
 
 void Messanger::displayText(const std::string& text) {
-    std::cout << text << std::endl;
+std::cout << text << std::endl;
 }
 
 void Messanger::displayMessage(const std::string& senderUsername, const std::string& message) {
@@ -15,5 +20,17 @@ void Messanger::displayMessage(const std::string& senderUsername, const std::str
 }
 
 void Messanger::sendMessage(const std::string& message) {
+    networking.send(message);
+}
 
+void Messanger::listenForInput() {
+    while (true) {
+        try {
+            std::string message;
+            std::cin >> message;
+            sendMessage(message);
+        } catch(const std::exception e) {
+            throw std::runtime_error(std::string("[Input error]: ") + e.what());
+        }
+    }
 }
