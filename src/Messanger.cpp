db4d674170fbe01733e7211cb0652a6039877705
AlonKufsa <alon.gal.kufsa@gmail.com>
2026-07-02 15:31:27 +0300

    Added messanger implementation

--- src/Messanger.cpp
+++ src/Messanger.cpp
@@ -5,6 +5,14 @@ Messanger::Messanger() {
 
 }
 
-void Messanger::displayText(const std::string message) {
-    std::cout << message << std::endl;
+void Messanger::displayText(const std::string text) {
+    std::cout << text << std::endl;
+}
+
+void Messanger::displayMessage(const std::string senderUsername, const std::string message) {
+    displayText(senderUsername + ": " + message);
+}
+
+void Messanger::sendMessage(const std::string message) {
+    
 }
